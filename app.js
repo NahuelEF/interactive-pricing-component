1655f7d5d5f3d7646aca0a0ace65b2b3e593cafc
NahuelEF <nahuelfedele@gmail.com>
2023-03-19 03:16:03 -0300

    Add: javascript input range

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
 const range = document.getElementById("range");
+const pageview = document.getElementById("pageview");
+const price = document.getElementById("price");
+const discount = document.getElementById("discount");
+
+const pageviews = [
+  { pageviews: "10K", price: 8 },
+  { pageviews: "50K", price: 12 },
+  { pageviews: "100K", price: 16 },
+  { pageviews: "500k", price: 24 },
+  { pageviews: "1M", price: 36 },
+];
+
+discount.addEventListener("change", () => {
+  discount.checked;
+});
 
 range.addEventListener("input", () => {
-  let x = (range.value * 100) / 4;
+  let num = range.value;
+  let x = (num * 100) / 4;
   range.style.background = `linear-gradient(
     90deg,
     var(--soft-cyan-full-slider) ${x}%,
     var(--light-grayish-blue-slider) ${x}%
   )`;
+
+  setValue(num);
 });
 
-/* 
-- 10K pageviews / $8 per month
-- 50K pageviews / $12 per month
-- 100K pageviews / $16 per month
-- 500k pageviews / $24 per month
-- 1M pageviews / $36 per month
-*/
-
-/* 
-const pageviews = [
-    { pageviews: "10K", price: 8 },
-    { pageviews: "50K", price: 12 },
-    { pageviews: "100K", price: 16 },
-    { pageviews: "500k", price: 24 },
-    { pageviews: "1M", price: 36 },
-  ];
-*/
+const setValue = (value) => {
+  pageview.textContent = pageviews[value].pageviews;
+  price.textContent = `$${pageviews[value].price}.00`;
+};
